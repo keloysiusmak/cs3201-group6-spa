c82eaf8b465a27f603e2c228881b82eb9e9c7b09
Quek Kai Yu <kaiyu92@hotmail.com>
2018-02-12 02:27:39 +0800

    QueryObject Implementation

---
+++ SPA/Pattern.cpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Pattern.h"
+
+using namespace std;
+
+Pattern::Pattern() {
+
+}
+
+void Pattern::setPattern(string entityType, string leftParam, string rightParam) {
+
+}
+
+string Pattern::getEntityType() {
+	return _entityType;
+}
+
+string Pattern::getLeftParam() {
+	return _leftParam;
+}
+
+string Pattern::getRightParam() {
+	return _rightParam;
+}
