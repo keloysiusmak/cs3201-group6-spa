81c6411547132cb00babfc19f93bf051695952cf
Shi Tian <shitian007@hotmail.com>
2018-02-17 01:05:52 +0800

    Setup follows conditionals boilerplate code

--- SPA/Evaluator.cpp
+++ SPA/Evaluator.cpp
@@ -5,6 +5,9 @@ using namespace std;
 string FOLLOWS = "follows";
 string FOLLOWSTAR = "followstar";
 
+string STMT_SYN = "stmtSyn";
+string STMT_NUM = "stmtNumber";
+
 Evaluator::Evaluator(QueryObject queryObj) {
 	queryObject = queryObj;
 };
@@ -30,7 +33,28 @@ ClauseResults Evaluator::evaluateClause(Clause clause) {
 };
 
 ClauseResults Evaluator::evaluateFollows(Clause clause) {
+
 	ClauseResults FollowsResults = ClauseResults();
+	Param firstParam = clause.getFirstParam();
+	Param secondParam = clause.getSecondParam();
+
+	if (clause.getFirstParam().type == STMT_SYN) {
+		if (clause.getSecondParam().type == STMT_SYN) {
+			//getAllFollows()
+		}
+		else if (clause.getSecondParam().type == STMT_NUM) {
+			//getFollowsBefore(secondParam.value)
+		} else {}
+	}
+	else if (clause.getFirstParam().type == STMT_NUM) {
+		if (clause.getSecondParam().type == STMT_SYN) {
+			//getFollowsAfter(firstParam.value)
+		}
+		else if (clause.getSecondParam().type == STMT_NUM) {
+			//checkFollows(firstParam.value, secondParam.value)
+		} else {}
+	} else {}
+
 	return FollowsResults;
 };
 
