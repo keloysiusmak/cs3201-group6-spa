47252633ee476e42df52d3efed24241de15860f9
hyma <e0036595@u.nus.edu>
2018-02-19 01:40:19 +0800

    Fix syntax errors for parent clause evaluator

--- SPA/Parent.cpp
+++ SPA/Parent.cpp
@@ -8,14 +8,14 @@ Parent::Parent()
 }
 
 //get Parent table result from PKB
-array<int, list<int>> Parent::getParentTable() {
-	return tableOne; //or parentTable?
-}
+//array<int, list<int>> Parent::getParentTable() {
+	//return tableOne; //or parentTable?
+//}
 
 bool Parent::isFirstParamValid(int firstParam, int secondParam) {
 	//check if parent exists in tableOne/parentTable
 	//return iterator to the end if parent doesn't exist in tableOne
-	if (tableOne.get(firstParam) == tableOne.end()) {
+	if (tableOne.find(firstParam) == tableOne.end()) {
 		return false;
 	}
 	else {
@@ -25,7 +25,7 @@ bool Parent::isFirstParamValid(int firstParam, int secondParam) {
 
 //if there is a reverse parent table
 bool Parent::isSecondParamValid(int firstParam, int secondParam) {
-	if (tableOne.get(SecondParam) == tableOneReverse.end()) {
+	if (tableOne.find(secondParam) == tableOneReverse.end()) {
 		return false;
 	}
 	else {
@@ -37,28 +37,28 @@ bool Parent::isSecondParamValid(int firstParam, int secondParam) {
 bool Parent::isParent(int firstParam, list<int> secondParam) {
 	list<int> parentOfVector = tableOne.at(firstParam);
 	//check if secondParam is in the parentOf list of firstParam
-	if ((parentOfVector.begin(), parentOfVector.end(), secondParam) != parentOfVector.end()) {
-		return true;
-	}
-	else {
-		return false;
-	}
+	//if ((parentOfVector.begin(), parentOfVector.end(), secondParam) != parentOfVector.end()) {
+		//return true;
+	//}
+	//else {
+		//return false;
+	//}
 }
 
 //if only first param is given
 list<int> Parent::getChildren(int firstParam) {
 	list<int> parentOfList = tableOne.at(firstParam);
-	return list<int>;
+	//return list<int>;
 }
 
 //if only second param is given
-int Parent::getParent(int secondParam) {
-	int parentOf = tableOneReverse.at(secondParam);
+list<int> Parent::getParent(int secondParam) {
+	list<int> parentOf = tableOne.at(secondParam);
 	return parentOf;
 }
 
 //if none of the params are given
 list<int, int> Parent::getAllParentPairs() {
 	//tableOne.begin, tableOne.end
-	return list<int, int>;
+	//return list of int;
 }
