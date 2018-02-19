ca195dd6782057394c960cf0477ba8ec1c161c8e
Keloysius Mak <sius_max@hotmail.com>
2018-02-19 01:25:21 +0000

    Add Follows functionality Add getFromTable functionality Modify insertToTable functionality

--- source/PKB.cpp
+++ source/PKB.cpp
@@ -4,14 +4,11 @@ using namespace std;
 
 #include "PKB.h"
 
-bool PKB::insertToTable(int table_id, string key_id, std::vector<int> column_id, std::vector<std::vector<string>> value)
+bool PKB::insertToTable(int table_id, std::string key_id, std::vector<std::vector<std::string>> value)
 {
-	unordered_map<string, std::vector<std::vector<string>>> table = tables[table_id];
+	unordered_map<std::string, std::vector<std::vector<std::string>>> table = tables[table_id];
 
-	if (column_id.size() != value.size())
-		return false;
-
-	std::unordered_map<string, std::vector<std::vector<string>>>::const_iterator got = table.find(key_id);
+	std::unordered_map<std::string, std::vector<std::vector<std::string>>>::const_iterator got = table.find(key_id);
 	/* Row does not exist */
 	if (got == table.end()) {
 		int tableValuesCount;
@@ -33,17 +30,10 @@ bool PKB::insertToTable(int table_id, string key_id, std::vector<int> column_id,
 		}
 
 		/* Return false if the supplied arguments does not equal to the number of columns specified in the PKB documentation*/
-		if (column_id.size() != tableValuesCount)
+		if (value.size() != tableValuesCount)
 			return false;
 
-		/* Return false if the supplied argument for column_id is not in increasing order */
-		for (int i = 0; i < tableValuesCount - 1; i++) {
-			if (column_id[i] > column_id[i+1]) {
-				return false;
-			}
-		}
-
-		std::vector<std::vector<string>> tableValues;
+		std::vector<std::vector<std::string>> tableValues;
 		for (int i = 0; i < tableValuesCount; i++) {
 			tableValues.push_back(value[i]);
 		}
@@ -52,38 +42,39 @@ bool PKB::insertToTable(int table_id, string key_id, std::vector<int> column_id,
 	}
 	/* Row exists */
 	else {
-		std::vector<std::vector<string>> tableValues = got->second;
-		std::vector<string> data;
+		std::vector<std::vector<std::string>> tableValues = got->second;
+		std::vector<std::string> data;
 
-		for (int i = 0; i < column_id.size(); i++) {
-			data = tableValues[column_id[i]];
+		for (int i = 0; i < value.size(); i++) {
+			data = tableValues[i];
 
 			/* Insert every element in vector */
 			for (int j = 0; j < value[i].size(); j++) {
 				data.push_back(value[i][j]);
 			}
-			tableValues[column_id[i]] = data;
+			tableValues[i] = data;
 		}
 	}
 
 	
 }
 
-std::vector<std::vector<string>> PKB::getFromTable(int table_id, string key_id)
+std::vector<std::vector<std::string>> PKB::getFromTable(int table_id, std::string key_id)
 {
-	std::vector<std::vector<string>> data;
+	unordered_map<std::string, std::vector<std::vector<std::string>>> table = tables[table_id];
 
-	return data;
+	std::unordered_map<std::string, std::vector<std::vector<std::string>>>::const_iterator got = table.find(key_id);
+	/* Row does not exist */
+	if (got == table.end()) {
+		std::vector<std::vector<std::string>> data;
+		return data;
+	}
+	else {
+		return got->second;
+	}
 }
 
 int main()
 {
 	PKB pkb;
-	std::vector<string> columns;
-	std::vector<string> values;
-	std::vector<std::vector<string>> values_columns;
-	columns.push_back("column1");
-	values.push_back("v");
-	values_columns.push_back(values);
-	pkb.insertToTable(1, "0", columns, values_columns);
 };
--- source/PKB.h
+++ source/PKB.h
@@ -10,12 +10,21 @@ using namespace std::tr1;
 
 class PKB {
 public:
-	/*PKB Structure*/
-	unordered_map<string, std::vector<std::vector<string>>> tables[6];
+	/*  PKB Structure*/
+	unordered_map<std::string, std::vector<std::vector<std::string>>> tables[6];
 
-	/*PKB Operations*/
-	bool insertToTable(int table_id, string key_id, std::vector<int> column_id, std::vector<std::vector<string>> value);
-	std::vector<std::vector<string>> getFromTable(int table_id, string key_id);
+	/* PKB Operations */
+	bool insertToTable(int, std::string, std::vector<std::vector<std::string>>);
+	std::vector<std::vector<std::string>> getFromTable(int, std::string);
 	
-	/**/
+	/* Follows Operations */
+	int getFollowsBefore(int);
+	int getFollowsAfter(int);
+	std::vector<int> getFollowsBeforeStar(int);
+	std::vector<int> getFollowsAfterStar(int);
+	bool checkFollows(int, int);
+	bool checkFollowsStar(int, int);
+	std::vector<int[2]> getAllFollows();
+	std::vector<unordered_map<int, std::vector<int>>> getAllFollowsStar();
+
 };
