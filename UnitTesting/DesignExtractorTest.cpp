00c15500ca54cccd9ecceb5fc9a5b9225fc18383
Keloysius Mak <sius_max@hotmail.com>
2018-03-15 14:48:28 +0000

    Add new DesignExtractor Tests

--- UnitTesting/DesignExtractorTest.cpp
+++ UnitTesting/DesignExtractorTest.cpp
@@ -22,25 +22,28 @@ namespace UnitTesting
 			pkb.insertToTable(STATEMENT_TABLE, 2, { { 1 },{},{},{1} });
 			pkb.insertToTable(STATEMENT_TABLE, 3, { { 1,2 },{},{},{2} });
 			pkb.insertToTable(STATEMENT_TABLE, 4, { { 2,3,4 },{},{},{ 3 } });
-			pkb.insertToTable(STATEMENT_TABLE, 5, { { 3 },{},{},{ 1 } });
+			pkb.insertToTable(STATEMENT_TABLE, 5, { { 3 },{},{},{ 4 } });
 			pkb.insertToTable(STATEMENT_TABLE, 6, { { 4,5 },{},{},{ 2 } });
-			pkb.insertToTable(STATEMENT_TABLE, 7, { { 5 },{},{},{ 1 } });
+			pkb.insertToTable(STATEMENT_TABLE, 7, { { 5 },{},{},{ 4 } });
+			pkb.insertToTable(STATEMENT_TABLE, 8, { { 6 },{1,2},{3},{ 1 } });
+			pkb.insertToTable(STATEMENT_TABLE, 9, { { 7 },{4,5},{6},{ 1 } });
 			pkb.insertToTable(STATEMENT_LIST_TABLE, 1, { {0}, {1,2,3}, {} });
 			pkb.insertToTable(STATEMENT_LIST_TABLE, 2, { {3},{ 4 },{} });
 			pkb.insertToTable(STATEMENT_LIST_TABLE, 3, { {4},{ 5 },{} });
 			pkb.insertToTable(STATEMENT_LIST_TABLE, 4, { { 4 },{ 6 },{} });
 			pkb.insertToTable(STATEMENT_LIST_TABLE, 5, { { 6 },{ 7 },{} });
+			pkb.insertToTable(STATEMENT_LIST_TABLE, 6, { { 0 },{ 8 },{} });
+			pkb.insertToTable(STATEMENT_LIST_TABLE, 7, { { 0 },{ 9 },{} });
 			pkb.insertToTable(PROC_INFO_TABLE, 1, { { 1 },{ },{} });
-			pkb.insertToTable(PROC_INFO_TABLE, 2, { { 1 },{},{} });
-			pkb.insertToTable(PROC_INFO_TABLE, 3, { { 1 },{},{} });
-			pkb.insertToTable(PROC_INFO_TABLE, 4, { { 1 },{},{} });
-			pkb.insertToTable(CALLS_TABLE, 1, { {2}, {4} });
-			pkb.insertToTable(CALLS_TABLE, 2, { { 3, 4 },{5} });
+			pkb.insertToTable(PROC_INFO_TABLE, 2, { { 6 },{},{} });
+			pkb.insertToTable(PROC_INFO_TABLE, 3, { { 7 },{},{} });
+			pkb.insertToTable(CALLS_TABLE, 1, { {2,3}, {} });
+			pkb.insertToTable(CALLS_TABLE, 2, { { 3 },{5} });
+			pkb.insertToTable(CALLS_TABLE, 3, { { },{ 7 } });
 
 			pkb.insertToNameTable(PROC_TABLE, {"a"});
 			pkb.insertToNameTable(PROC_TABLE, { "b" });
 			pkb.insertToNameTable(PROC_TABLE, { "c" });
-			pkb.insertToNameTable(PROC_TABLE, { "d" });
 			de = DesignExtractor();
 		}
 		TEST_METHOD(DesignExtractorExtract)
@@ -143,25 +146,40 @@ namespace UnitTesting
 			std::vector<std::vector<int>> result;
 			Assert::AreEqual(true, (result == pkb.getFromTable(CALLS_INVERSE_TABLE, 1)));
 			Assert::AreEqual(1, pkb.getFromTable(CALLS_INVERSE_TABLE, 2)[0][0]);
-			Assert::AreEqual(2, pkb.getFromTable(CALLS_INVERSE_TABLE, 3)[0][0]);
-			Assert::AreEqual(2, pkb.getFromTable(CALLS_INVERSE_TABLE, 4)[0][0]);
+			Assert::AreEqual(1, pkb.getFromTable(CALLS_INVERSE_TABLE, 3)[0][0]);
+			Assert::AreEqual(2, pkb.getFromTable(CALLS_INVERSE_TABLE, 3)[0][1]);
+			result.clear();
+			Assert::AreEqual(true, (result == pkb.getFromTable(CALLS_INVERSE_TABLE, 4)));
 		}
 		TEST_METHOD(DesignExtractorExtractCalls)
 		{
 			de.extract(pkb);
 			std::vector<std::vector<int>> result;
-			result = { { 2,3,4} };
+			result = { { 2,3} };
 			Assert::AreEqual(true, (result == pkb.getFromTable(CALLS_STAR_TABLE, 1)));
-			result = { { 3, 4 } };
+			result = { { 3 } };
 			Assert::AreEqual(true, (result == pkb.getFromTable(CALLS_STAR_TABLE, 2)));
 			result = { { 1,2 } };
 			Assert::AreEqual(true, (result == pkb.getFromTable(CALLS_STAR_INVERSE_TABLE, 3)));
-			result = { { 1,2 } };
+			result.clear();
 			Assert::AreEqual(true, (result == pkb.getFromTable(CALLS_STAR_INVERSE_TABLE, 4)));
 			result = { { 1 } };
 			Assert::AreEqual(true, (result == pkb.getFromTable(CALLS_STAR_INVERSE_TABLE, 2)));
 			result.clear();
 			Assert::AreEqual(true, (result == pkb.getFromTable(CALLS_STAR_INVERSE_TABLE, 1)));
+		}
+		TEST_METHOD(DesignExtractorExtractUsesModifies)
+		{
+			de.extract(pkb);
+			std::vector<int> result;
+			std::vector<int> a = pkb.getFromTable(STATEMENT_TABLE, 5)[1];
+			for (int i = 0; i < a.size(); i++) {
+				Logger::WriteMessage(to_string(i).c_str());
+			}
+			result = { { 1,2,4,5 } };
+			Assert::AreEqual(true, (result == pkb.getFromTable(STATEMENT_TABLE, 5)[1]));
+			result = { { 3,6 } };
+			Assert::AreEqual(true, (result == pkb.getFromTable(STATEMENT_TABLE, 5)[2]));
 		}
 	};
 }
