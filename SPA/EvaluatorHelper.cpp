a639eb0f34a38b613a33ff84d5a918cc541b6365
Shi Tian <shitian007@hotmail.com>
2018-03-31 13:37:43 +0800

    Setup skeleton for synonym group sorting

--- SPA/EvaluatorHelper.cpp
+++ SPA/EvaluatorHelper.cpp
@@ -2,6 +2,77 @@
 
 using namespace std;
 
+void EvaluatorHelper::consolidateClauses(vector<Clause> const &clauses, vector<Clause> &consolidated) {
+	for (Clause clause : clauses) { consolidated.push_back(clause); } }
+void EvaluatorHelper::consolidateClauses(vector<Pattern> const &clauses, vector<Clause> &consolidated) {
+	for (Clause clause : clauses) { consolidated.push_back(clause); } }
+
+// Preliminary sorting by synonyms 
+vector<vector<Clause>> EvaluatorHelper::sortIntoGroups(vector<Clause> &clauses) {
+	map<Param, int> paramsHash = ufdsParams(clauses);
+};
+
+map<Param, int> EvaluatorHelper::ufdsParams(vector<Clause> &clauses) {
+	// Hash all params
+	map<Param, Node> paramsHash;
+	for (Clause clause : clauses) {
+		Param lhs = clause.getLeftParam();
+		Param rhs = clause.getRightParam();
+		if (Utils::isSynonym(lhs) && lhs.type != ALL) {
+			Node newNode = Node(lhs);
+			paramsHash[lhs] = newNode;
+		}
+		if (Utils::isSynonym(rhs) && lhs.type != ALL) {
+			Node newNode = Node(rhs);
+			paramsHash[lhs] = newNode;
+		}
+		if (clause.getRelRef() == None) { // Check for pattern
+			Pattern* p = reinterpret_cast<Pattern*>(&clause);
+			Param patternEnt = p->getEntity();
+			Node newNode = Node(patternEnt);
+			paramsHash[patternEnt] = newNode;
+		} else { ; }
+	}
+
+	// Group according to clauses
+	for (Clause clause : clauses) {
+		Param lhs = clause.getLeftParam();
+		Param rhs = clause.getRightParam();
+		if (Utils::isSynonym(lhs) && lhs.type != ALL &&
+			Utils::isSynonym(rhs) && rhs.type != ALL) {
+			unionParams(lhs, rhs, paramsHash);
+		}
+	}
+
+	//for (pair<Param, Node> paramNode : paramsHash) {
+	//	
+	//}
+
+	map<Param, int> paramGroups;
+	return paramGroups;
+
+};
+
+// Unions two given params
+void EvaluatorHelper::unionParams(Param p1, Param p2, map<Param, Node> &paramsHash) {
+	Node p1Node = paramsHash[p1];
+	Node p2Node = paramsHash[p2];
+	
+	Node p1NodeSet = findSet(p1Node, paramsHash);
+	Node p2NodeSet = findSet(p2Node, paramsHash);
+
+	if (!Utils::isSameParam(p1NodeSet.self, p2NodeSet.self)) {
+		p1NodeSet.parent = &p2NodeSet;
+	}
+};
+
+// Finds the Set of the given node
+Node EvaluatorHelper::findSet(Node &n, map<Param, Node> &paramsHash) {
+	if (!n.hasParent) return n;
+	Node parent = findSet(*n.parent, paramsHash);
+	return *n.parent;
+}
+
 /* Merges the clauseResults into the intermediate table */
 void EvaluatorHelper::mergeClauseTable(ClauseResults &clauseResults, IntermediateTable &iTable) {
 	if (clauseParamsInTable(clauseResults, iTable)) {
@@ -114,10 +185,10 @@ void EvaluatorHelper::addClauseParamToTable(ClauseResults &clauseResults, Interm
 /* Returns number of params of With Clause in table */
 int EvaluatorHelper::withClauseNumSyns(Clause &clause, IntermediateTable &iTable) {
 	int numSyns = 0;
-	Param lhs = clause.getFirstParam();
-	Param rhs = clause.getSecondParam();
+	Param lhs = clause.getLeftParam();
+	Param rhs = clause.getRightParam();
 	if (iTable.getParamIndex(lhs) > -1) numSyns++; // Assume syn param in table
 	if (iTable.getParamIndex(rhs) > -1) numSyns++; // Assume syn param in table
 	return numSyns;
-}
+};
 
--- SPA/EvaluatorHelper.h
+++ SPA/EvaluatorHelper.h
@@ -4,15 +4,39 @@
 #include "IntermediateTable.h"
 #include <list>
 
+// UFDS Helper
+struct Node {
+	Param self;
+	Node *parent;
+	bool hasParent;
+
+	Node() { ; };
+	Node(Param self) {
+		this->self = self;
+	};
+
+	void setParent(Node &node) {
+		this->parent = &node;
+	}
+};
+
 class EvaluatorHelper {
 
- public:
-  static void mergeClauseTable(ClauseResults &clauseResults, IntermediateTable &iTable);
-  static void mergeWithoutOverlap(ClauseResults &clauseResults, IntermediateTable &iTable);
-  static void mergeWithOverlap(ClauseResults &clauseResults, IntermediateTable &iTable);
+public:
+	static void consolidateClauses(vector<Clause> const &clauses, vector<Clause> &consolidated);
+	static void consolidateClauses(vector<Pattern> const &clauses, vector<Clause> &consolidated);
+	static vector<vector<Clause>> sortIntoGroups(vector<Clause> &clauses);
 
-  static bool clauseParamsInTable(ClauseResults &clauseResults, IntermediateTable &iTable);
-  static void addClauseParamToTable(ClauseResults &clauseResults, IntermediateTable &iTable);
+	static map<Param, int> ufdsParams(vector<Clause> &clauses);
+	static void unionParams(Param p1, Param p2, map<Param, Node> &paramsHash);
+	static Node findSet(Node &n, map<Param, Node> &paramsHash);
 
-  static int withClauseNumSyns(Clause &clause, IntermediateTable &iTable);
+	static void mergeClauseTable(ClauseResults &clauseResults, IntermediateTable &iTable);
+	static void mergeWithoutOverlap(ClauseResults &clauseResults, IntermediateTable &iTable);
+	static void mergeWithOverlap(ClauseResults &clauseResults, IntermediateTable &iTable);
+
+	static bool clauseParamsInTable(ClauseResults &clauseResults, IntermediateTable &iTable);
+	static void addClauseParamToTable(ClauseResults &clauseResults, IntermediateTable &iTable);
+
+	static int withClauseNumSyns(Clause &clause, IntermediateTable &iTable);
 };
