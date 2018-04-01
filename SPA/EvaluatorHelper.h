3e17af367d823bfe95f21fbb7af42a9d1c960c03
Shi Tian <shitian007@hotmail.com>
2018-04-01 15:20:56 +0800

    Refactor Evaluator Helper

--- SPA/EvaluatorHelper.h
+++ SPA/EvaluatorHelper.h
@@ -4,33 +4,10 @@
 #include "IntermediateTable.h"
 #include <list>
 
-// UFDS Helper
-struct Node {
-	Param self;
-	Node *parent;
-	bool hasParent;
-
-	Node() { ; };
-	Node(Param self) {
-		this->self = self;
-	};
-
-	void setParent(Node &node) {
-		this->parent = &node;
-	}
-};
 
 class EvaluatorHelper {
 
 public:
-	static void consolidateClauses(vector<Clause> const &clauses, vector<Clause> &consolidated);
-	static void consolidateClauses(vector<Pattern> const &clauses, vector<Clause> &consolidated);
-	static vector<vector<Clause>> sortIntoGroups(vector<Clause> &clauses);
-
-	static map<Param, int> ufdsParams(vector<Clause> &clauses);
-	static void unionParams(Param p1, Param p2, map<Param, Node> &paramsHash);
-	static Node findSet(Node &n, map<Param, Node> &paramsHash);
-
 	static void mergeClauseTable(ClauseResults &clauseResults, IntermediateTable &iTable);
 	static void mergeWithoutOverlap(ClauseResults &clauseResults, IntermediateTable &iTable);
 	static void mergeWithOverlap(ClauseResults &clauseResults, IntermediateTable &iTable);
