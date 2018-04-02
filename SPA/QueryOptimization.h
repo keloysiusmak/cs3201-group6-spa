e55eb61fd0670b642d35c3bb2efa3cac88c5f037
hymss <hyma.tiyyagura@gmail.com>
2018-04-02 19:46:11 +0800

    Modify grouping by num of constants to suit new input type

--- SPA/QueryOptimization.h
+++ SPA/QueryOptimization.h
@@ -32,8 +32,7 @@ public:
 
 	static map<int, vector<Clause>> sortIntoGroups(vector<Clause> &clauses);
 	static map<int, vector<Clause>> groupClauses(vector<Clause> &clauses, map<Param, Node> &paramsHash);
-	//static map<int, vector<Clause>> finalGroupClauses(map<int, vector<Clause>> groupClauses);
-	//static map<int, vector<Clause>> sortByNumConstants(map<int, vector<Clause>> groupedClauses);
+	//static map<int, vector<Clause>> numConstantsGroupClauses(vector<Clause> groupedClauses);
 	static map<Param, Node> ufdsParams(vector<Clause> &clauses);
 	static void unionParams(Param &p1, Param &p2, map<Param, Node> &paramsHash);
 	static Node* findSet(Node &n, map<Param, Node> &paramsHash);
