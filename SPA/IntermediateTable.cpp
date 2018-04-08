16504f45d960a9e667444857ea253cc331b4f668
Shi Tian <shitian007@hotmail.com>
2018-04-08 15:45:33 +0800

    Add hasResults to IntermediateTable

--- SPA/IntermediateTable.cpp
+++ SPA/IntermediateTable.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void IntermediateTable::instantiateTable() {
-	hasResults = true;
+	hasResults = false;
 };
 
 void IntermediateTable::setTableParams(vector<Param> params) {
@@ -24,6 +24,13 @@ void IntermediateTable::addTableParams(Param p) {
 void IntermediateTable::setResultsTable(vector<vector<int>> &table) {
 	resultsTable = table;
 };
+
+bool IntermediateTable::tableHasResults() {
+	if (tableParams.size() > 0) {
+		return (resultsTable.size() > 0);
+	}
+	else return hasResults;
+}
 
 int IntermediateTable::getParamIndex(Param p) {
 	if (tableParams.find(p) != tableParams.end()) return tableParams[p];
