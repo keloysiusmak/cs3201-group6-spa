3cef13153e804d33e52b6a27929376b3c43a509b
Keloysius Mak <sius_max@hotmail.com>
2018-04-13 15:00:09 +0800

    Fix run-tests

---
+++ run-tests.py
@@ -0,0 +1,26 @@
+import os
+
+# SYSTEM SOURCES
+i = 1
+print "\nStarting Test Cases\n---"
+while (i <= 23):
+    print "Running Test Case "+str(i)
+    os.system("\Release\AutoTester Test-Cases/sources/testcase"+str(i)+"-source.txt Test-Cases/queries/testcase"+str(i)+"-queries.txt Test-Cases/output/testcase"+str(i)+"-out.xml")
+    i = i + 1
+
+i = 1
+print "---\nCompleted Test Cases"
+print "\nStarting Blackbox\n---"
+while (i <= 2):
+    print "Running Blackbox "+str(i)
+    os.system("\Release\AutoTester Test-Cases/sources/blackbox"+str(i)+"-source.txt Test-Cases/queries/blackbox"+str(i)+"-queries.txt Test-Cases/output/blackbox"+str(i)+"-out.xml")
+    i = i + 1
+
+i = 1
+print "---\nCompleted Blackbox"
+print "\nStarting Extension\n---"
+while (i <= 2):
+    print "Running Extension "+str(i)
+    os.system("\Release\AutoTester Test-Cases/sources/extension"+str(i)+"-source.txt Test-Cases/queries/extension"+str(i)+"-queries.txt Test-Cases/output/extension"+str(i)+"-out.xml")
+    i = i + 1
+print "---\nCompleted Extension"
