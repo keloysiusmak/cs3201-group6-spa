ea8e012d2329abc3a7498e584ddfbefd4a2e2db1
Keloysius Mak <sius_max@hotmail.com>
2018-04-13 14:56:37 +0800

    Update tests

--- AutoTester/run-tests.py
+++ AutoTester/run-tests.py
@@ -5,7 +5,7 @@ i = 1
 print "\nStarting Test Cases\n---"
 while (i <= 23):
     print "Running Test Case "+str(i)
-    os.system("AutoTester Test-Cases/sources/testcase"+str(i)+"-source.txt Test-Cases/queries/testcase"+str(i)+"-queries.txt Test-Cases/output/testcase"+str(i)+"-out.xml")
+    os.system("../Release/AutoTester Test-Cases/sources/testcase"+str(i)+"-source.txt Test-Cases/queries/testcase"+str(i)+"-queries.txt Test-Cases/output/testcase"+str(i)+"-out.xml")
     i = i + 1
 
 i = 1
@@ -13,7 +13,7 @@ print "---\nCompleted Test Cases"
 print "\nStarting Blackbox\n---"
 while (i <= 2):
     print "Running Blackbox "+str(i)
-    os.system("AutoTester Test-Cases/sources/blackbox"+str(i)+"-source.txt Test-Cases/queries/blackbox"+str(i)+"-queries.txt Test-Cases/output/blackbox"+str(i)+"-out.xml")
+    os.system("../Release/AutoTester Test-Cases/sources/blackbox"+str(i)+"-source.txt Test-Cases/queries/blackbox"+str(i)+"-queries.txt Test-Cases/output/blackbox"+str(i)+"-out.xml")
     i = i + 1
 
 i = 1
@@ -21,6 +21,6 @@ print "---\nCompleted Blackbox"
 print "\nStarting Extension\n---"
 while (i <= 2):
     print "Running Extension "+str(i)
-    os.system("AutoTester Test-Cases/sources/extension"+str(i)+"-source.txt Test-Cases/queries/extension"+str(i)+"-queries.txt Test-Cases/output/extension"+str(i)+"-out.xml")
+    os.system("../Release/AutoTester Test-Cases/sources/extension"+str(i)+"-source.txt Test-Cases/queries/extension"+str(i)+"-queries.txt Test-Cases/output/extension"+str(i)+"-out.xml")
     i = i + 1
 print "---\nCompleted Extension"
